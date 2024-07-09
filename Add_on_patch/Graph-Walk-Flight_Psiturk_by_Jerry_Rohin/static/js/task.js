23b1bbdb1794d9533eafb5c6716962f111b0d16c
JerryGuo2001 <138930979+JerryGuo2001@users.noreply.github.com>
2024-07-09 16:39:15 -0700

    uploading psiturk

---
+++ Add_on_patch/Graph-Walk-Flight_Psiturk_by_Jerry_Rohin/static/js/task.js
@@ -0,0 +1,112 @@
+var debug_mode = 0; // debug mode determines how long the blocks are, 5 sec in debug mode, 5 minutes in actual experiment
+//var data_save_method = 'csv_server_py';
+var data_save_method = 'csv_server_py';
+
+// Will be set to true when experiment is exiting fullscreen normally, to prevent above end experiment code
+var normal_exit = false;
+var window_height = window.screen.height;
+
+
+
+//this is to test if the user leave the webpage
+var detectfocus=0
+var isinfocus=1
+document.addEventListener('mouseleave', e=>{
+  detectfocus=detectfocus+1
+  isinfocus=0
+  //this is to see if user are focus or not
+})
+document.addEventListener('visibilitychange', e=>{
+   if (document.visibilityState === 'visible') {
+ //report that user is in focus
+ isinfocus=1
+  } else {
+  detectfocus=detectfocus+1
+  isinfocus=0
+  //this is to see if user are focus or not
+  }  
+})
+
+// Randomly generate an 8-character alphanumeric subject ID via jsPsych
+var subject_id = jsPsych.randomization.randomID(8);
+
+// Load PsiTurk
+var psiturk = new PsiTurk(uniqueId, adServerLoc, mode);
+var condition = psiturk.taskdata.get('condition') + 1; // they do zero-indexing
+
+var timeline = []
+
+//welcome page
+var welcome = {
+  type: 'survey-html-form',
+  html: " <label for='worker_id'>Enter your Prolific Worker ID. Please make sure this is correct! </label><br><input type='text' id='worker_id' name='worker_id' required><br><br>",
+  on_finish: function (data) {
+    data.trial_type = "id_enter"
+    window.useridtouse=data.responses
+    window.useridtouse = useridtouse.split('"')[3];
+  }
+}
+//welcome page end
+
+//Goal directed planning
+function createPhase3(numberoftrial=3,){
+  let phase3 = {}
+  for (let i = 0; i < numberoftrial; i++){
+    if (i==numberoftrial-1){
+      phase3[i] = {
+        type: 'html-keyboard-response',
+        choices: ['space'],
+        stimulus: `<p>hi ${i}</p>`,
+        on_finish: function (data) {
+          data.trial_type = 'intro_1';
+          jsPsych.addNodeToEndOfTimeline({
+            timeline: [thank_you],
+          }, jsPsych.resumeExperiment)
+        }
+      }
+    }else{
+      phase3[i] = {
+        type: 'html-keyboard-response',
+        choices: ['space'],
+        stimulus: `<p>hi ${i}</p>`,
+        on_finish: function (data) {
+          data.trial_type = 'intro_1';
+          jsPsych.addNodeToEndOfTimeline({
+            timeline: [phase3[i+1]],
+          }, jsPsych.resumeExperiment)
+        }
+      }
+    }
+  }
+  return phase3
+}
+
+phase3=createPhase3()
+
+//Goal directed planning end
+
+// final thank you
+var thank_you = {
+  type: 'html-keyboard-response',
+  choices: ['space'],
+  stimulus: "<p> Congratulations, you are all done!</p><p>The secret code to enter at the beginning screen is: AJFHBG897</p><p> Please make sure to submit the HIT and email mnadkarn@gmail.com if you had any issues! </p>",
+  on_finish: function (data) {
+    data.trial_type = 'thank_you';
+    data.detectfocus = detectfocus;
+    save_data(true)
+  }
+}
+timeline.push(welcome)
+timeline.push(phase3[0])
+
+jsPsych.init({
+  timeline: timeline,
+  preload_images: all_images,
+  max_load_time: 600000,
+  on_finish: function () {
+    /* Retrieve the participant's data from jsPsych */
+    // Determine and save participant bonus payment
+    psiturk.recordUnstructuredData("subject_id", subject_id);
+    save_data(true)
+  },
+})
