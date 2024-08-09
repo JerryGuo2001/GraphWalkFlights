e66c281ee32b70e3824c11391b5e3199a7722e6d
RohinPalsule <122756013+RohinPalsule@users.noreply.github.com>
2024-08-09 12:40:14 -0700

    Adjusted learning images

--- Graph-Walk-Flight_Psiturk_by_Jerry_Rohin/static/rooms/room_maker.js
+++ Graph-Walk-Flight_Psiturk_by_Jerry_Rohin/static/rooms/room_maker.js
@@ -7,7 +7,7 @@ function parse(str) {
 
 //Learning phase
 function create_learning_trial(room_choiceStims_left,room_choiceStims_right,trial_num) {
-  return parse("<img style='position:absolute;top: 50%;right: 75%;transform: translate(50%, -50%);z-score:0;width: 250px;height: 250px;' src='../static/images/%s' height='250'> <img style='position:absolute;top: 50%;right: 25%;transform: translate(50%, -50%);z-score:0;width: 250px;height: 250px;' src='../static/images/%s' height='250'><br><style>body {background-color: #ffff;}</style>"
+  return parse("<img style='position:absolute;top: 50%;right: 70%;transform: translate(50%, -50%);z-score:0;width: 350px;height: 350px;' src='../static/images/%s' height='350'> <img style='position:absolute;top: 50%;right: 30%;transform: translate(50%, -50%);z-score:0;width: 350px;height: 350px;' src='../static/images/%s' height='350'><br><style>body {background-color: #ffff;}</style>"
   ,room_choiceStims_left[trial_num],room_choiceStims_right[trial_num])
 }
 
