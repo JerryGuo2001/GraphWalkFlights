62e4383ba9281b958b8910b378921d29b777e0a2
JerryGuo2001 <jianleg@uci.edu>
2025-04-30 18:28:07 -0700

    update code

--- GWF_recognition_test/static/js/task.js
+++ GWF_recognition_test/static/js/task.js
@@ -756,7 +756,7 @@ var comments = 0
 var thank_you = {
   type: 'html-keyboard-response',
   choices: ['space'],
-  stimulus: "<p> Congratulations, you are all done!</p><p>The secret code to enter at the beginning screen is: AJFHBG897</p><p> Please make sure to submit the HIT and email uciccnl@gmail.com if you had any issues! </p>",
+  stimulus: "<p> Congratulations, you are all done!</p><p>The secret code to enter at the beginning screen is: CIMBPENS</p><p> Please make sure to submit the HIT and email uciccnl@gmail.com if you had any issues! </p>",
   on_start:function(data){
     save_final_deter='final',
     save_data()
