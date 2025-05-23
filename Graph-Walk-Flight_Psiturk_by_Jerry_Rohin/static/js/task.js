2594385f28210fe905ed15ad0370f3eb5cc31d43
JerryGuo2001 <jianleg@uci.edu>
2025-05-22 19:31:34 -0700

    checked

--- Graph-Walk-Flight_Psiturk_by_Jerry_Rohin/static/js/task.js
+++ Graph-Walk-Flight_Psiturk_by_Jerry_Rohin/static/js/task.js
@@ -50,7 +50,6 @@ var welcome = {
     data.stimulus = "intro"
     data.cities = `${cityNameList.join("; ")}`;
     data.city_images = `${list_images.join("; ")}`
-    save_data()
   }
 }
 //welcome page end
