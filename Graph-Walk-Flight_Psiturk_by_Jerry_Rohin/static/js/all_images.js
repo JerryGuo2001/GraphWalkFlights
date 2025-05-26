6988c548f8ba5903fe6be10288cc13f9a51c176c
RohinPalsule <rohin.palsule@gmail.com>
2025-05-25 23:11:09 -0700

    added us map to preload

--- Graph-Walk-Flight_Psiturk_by_Jerry_Rohin/static/js/all_images.js
+++ Graph-Walk-Flight_Psiturk_by_Jerry_Rohin/static/js/all_images.js
@@ -1,4 +1,4 @@
-var instruction_images = ['short1.png','short2.png','short3.png','short4.png','short5.png','short6.png','Shortest.png',
+var instruction_images = ['short1.png','short2.png','short3.png','short4.png','short5.png','short6.png','Shortest.png','blankUSmap.png',
     'shortestarrow.png','shortestIMG.png','shortestinstruction.png','studyinstruction2.png','Austin.png','StLouis.png','LosAngeles.png',"NewYorkCity.png",'LasVegas.png','Detroit.png','Seattle.png','arrows.png'];
     
     instruction_images = instruction_images.map(img => "../static/images/" + img);
